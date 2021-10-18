80da15803816e753f0f3948ac95e7db89f08086f
Manuel Ivan Chavez Pacheco <75952342+Manuelchave@users.noreply.github.com>
2021-10-18 17:27:37 -0600

    Add files via upload

---
+++ Proyecto/src/main/java/com/example/Proyecto/repository/HerramientaRepository.java
@@ -0,0 +1,6 @@
+package com.example.Proyecto.repository;
+
+import com.example.Proyecto.model.Herramienta;
+import org.springframework.data.jpa.repository.*;
+public interface HerramientaRepository extends JpaRepository<Herramienta, Long> {
+}
